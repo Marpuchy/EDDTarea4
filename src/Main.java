82da64ab868be1e0c50af8fe9ff6bfc942f70467
Marc Ibàñez <mibanezp0709@gmail.com>
2024-02-14 10:33:52 +0100

    Documentada

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 
+/**
+ * Al introducir un número, el programa crea una lista de todos los primos desde el 0 hasta ese número
+ *
+ * @author Marc Ibáñez Puchol
+ * @version 1.0
+ * @since 14/02/2024
+ */
+
 public class Main {
+
+    /**
+     * Este es el método de la clase main
+     *
+     */
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Introduce el número para la criba de Erastótenes:");
@@ -20,7 +33,13 @@ public class Main {
 
     }
 
-    private static void ImprimirVector(int[] vector) {
+    /**
+     * Este es el metodo ImprimirVector
+     *
+     * @param vector array de int que representa el vector que debe recorrer e imprimir
+     */
+
+    public static void ImprimirVector(int[] vector) {
         for (int i = 0; i < vector.length; i++) {
             if (i % 10 == 0) System.out.println();
             System.out.print(i + 1 + "\t");
@@ -28,6 +47,13 @@ public class Main {
     }
 
     // Generar números primos de 1 a max
+
+    /**
+     * Este es el metodo GenerarPrimos
+     *
+     * @param max valor que corresponde al valor máximo al que debe llegar el array
+     * @return <p>Array de int con los primos generados durante el método</p>
+     */
     public static int[] GenerarPrimos(int max) {
         // int i, j; incorrecto, una sola declaración por línea
         // correcto
@@ -63,37 +89,65 @@ public class Main {
         }
     }
 
-    private static void Criba(int dim, boolean[] esPrimo) {
+    /**
+     * Este es el metodo Criba
+     *
+     * @param tamañoMaximo valor que corresponde al valor máximo al que debe llegar el array
+     * @param esPrimo      booleano que corresponde a si el número es primo o no
+     */
+    public static void Criba(int tamañoMaximo, boolean[] esPrimo) {
         int i;
-        for (i = 2; i < Math.sqrt(dim) + 1; i++) {
+        for (i = 2; i < Math.sqrt(tamañoMaximo) + 1; i++) {
             if (esPrimo[i]) {
                 // Eliminar los múltiplos de i
-                EliminarMultiplosDeUno(i, dim, esPrimo);
+                EliminarMultiplosDeUno(i, tamañoMaximo, esPrimo);
             }
         }
     }
 
-    private static void EliminarMultiplosDeUno(int i, int dim, boolean[] esPrimo) {
+    /**
+     * Este es el metodo EliminarMultiplosDeUno
+     *
+     * @param i            variable contador
+     * @param tamañoMaximo valor que corresponde al valor máximo al que debe llegar el array
+     * @param esPrimo      booleano que corresponde a si el número es primo o no
+     */
+    public static void EliminarMultiplosDeUno(int i, int tamañoMaximo, boolean[] esPrimo) {
         int j;
-        for (j = 2 * i; j < dim; j += i)
+        for (j = 2 * i; j < tamañoMaximo; j += i)
             esPrimo[j] = false;
     }
 
-    private static int ContarPrimos(int dim, boolean[] esPrimo) {
+    /**
+     * Este es el metodo ContarPrimos
+     *
+     * @param tamañoMaximo valor que corresponde al valor máximo al que debe llegar el array
+     * @param esPrimo      booleano que corresponde a si el número es primo o no
+     * @return <p>valor de el número de primos contados</p>
+     */
+    public static int ContarPrimos(int tamañoMaximo, boolean[] esPrimo) {
         int i;
         int cuenta = 0;
-        for (i = 0; i < dim; i++) {
+        for (i = 0; i < tamañoMaximo; i++) {
             if (esPrimo[i])
                 cuenta++;
         }
         return cuenta;
     }
 
-    private static int[] RellenarDePrimos(int cuenta, int dim, boolean[] esPrimo) {
+    /**
+     * Este es el metodo RellenarDePrimos
+     *
+     * @param cuenta valor que corresponde al número de primos que hay
+     * @param tamañoMaximo valor que corresponde al valor máximo al que debe llegar el array
+     * @param esPrimo      booleano que corresponde a si el número es primo o no
+     * @return <p>Array de int ya rellenado con los primos correspondientes</p>
+     */
+    public static int[] RellenarDePrimos(int cuenta, int tamañoMaximo, boolean[] esPrimo) {
         int j;
         int i;
         int[] primos = new int[cuenta];
-        for (i = 0, j = 0; i < dim; i++) {
+        for (i = 0, j = 0; i < tamañoMaximo; i++) {
             if (esPrimo[i])
                 primos[j++] = i;
         }
